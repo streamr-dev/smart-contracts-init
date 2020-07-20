6eda22fd958da6bae19d2f2563b3ea125bcbcd17
Jonathan Wolff <jwolff@isentropy.com>
2020-07-19 23:14:40 -0400

    docker in docker

--- Dockerfile
+++ Dockerfile
@@ -18,15 +18,16 @@ WORKDIR /home/node
 COPY ./ ./
 RUN npm ci --only=production
 
-FROM ubuntu:16.04
+FROM ubuntu:20.04
 ARG NODE_VERSION="v12.15.0"
 
 WORKDIR /
 ENV PATH="/node-$NODE_VERSION-linux-x64/bin:${PATH}"
 COPY --from=builder --chown=root:root /node-$NODE_VERSION-linux-x64/ /node-$NODE_VERSION-linux-x64/
 RUN useradd -ms /bin/bash node
+RUN apt-get update && apt-get install -y docker.io docker-compose jq
+RUN usermod -a -G docker node
 USER node
 WORKDIR /home/node
 COPY --from=builder --chown=node:node /home/node/ ./
-CMD node index.js
-
+CMD node index.js && ./bridge/start_bridge.sh
--- deploy_du2_factories.js
+++ deploy_du2_factories.js
@@ -21,8 +21,8 @@ class LoggingProvider extends JsonRpcProvider {
         });
     }
 }
-const provider_foreign = new JsonRpcProvider('http://127.0.0.1:8545');
-const provider_home = new JsonRpcProvider('http://127.0.0.1:8546');
+const provider_foreign = new JsonRpcProvider('http://10.200.10.1:8545');
+const provider_home = new JsonRpcProvider('http://10.200.10.1:8546');
 
 const wallet_home = new Wallet('0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0', provider_home)
 const wallet_foreign = new Wallet('0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0', provider_foreign)
