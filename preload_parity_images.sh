d127a9a798b4df7353d949a81a1a1511e3d7fadd
Jonathan Wolff <jwolff@isentropy.com>
2020-09-15 21:34:56 -0400

    move parity_data to parity_data.default when init is finished

--- preload_parity_images.sh
+++ preload_parity_images.sh
@@ -5,6 +5,8 @@ streamr-docker-dev start
 streamr-docker-dev stop parity-sidechain-node0 parity-node0
 docker-compose up -d
 docker wait streamr-dev-smart-contracts-init
+docker exec streamr-dev-parity-sidechain-node0 /bin/bash -c 'mv /home/parity/parity_data /home/parity/parity_data.default'
+docker exec streamr-dev-parity-node0 /bin/bash -c 'mv /home/parity/parity_data /home/parity/parity_data.default'
 streamr-docker-dev stop
 docker commit streamr-dev-parity-sidechain-node0 streamr/open-ethereum-poa-sidechain-preload1:dev
 docker commit streamr-dev-parity-node0 streamr/open-ethereum-poa-mainchain-preload1:dev
