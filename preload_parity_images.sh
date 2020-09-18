f83eb101ed184e23cec416a58c41f1418cdeb748
Jonathan Wolff <jwolff@isentropy.com>
2020-09-18 14:28:35 +0200

    update contracts, check smart-contract-init return code

--- preload_parity_images.sh
+++ preload_parity_images.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -eux
 cd `dirname $0`
+LOG=smartContractInit.log
 docker build -t streamr/smart-contracts-init:dev .
 streamr-docker-dev start
 streamr-docker-dev stop parity-sidechain-node0 parity-node0
 docker-compose up -d
-docker wait streamr-dev-smart-contracts-init
+INITSTATUS=`docker wait streamr-dev-smart-contracts-init`
+docker logs streamr-dev-smart-contracts-init > $LOG
+echo "streamr-dev-smart-contracts-init finished with status $INITSTATUS. Logs in $LOG"
+test $INITSTATUS -ne 0 && echo "streamr-dev-smart-contracts-init failed" && exit 1
 docker exec streamr-dev-parity-sidechain-node0 /bin/bash -c 'mv /home/parity/parity_data /home/parity/parity_data.default'
 docker exec streamr-dev-parity-node0 /bin/bash -c 'mv /home/parity/parity_data /home/parity/parity_data.default'
 streamr-docker-dev stop
