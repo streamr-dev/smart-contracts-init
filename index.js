bde92d7b49cb713a45330246a3d8a7faa24606dd
Juuso Takalainen <juuso@streamr.network>
2021-06-03 16:34:01 +0300

    some cleanup

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {
 } = require("ethers")
 
 
-const TokenJson = require("./TestToken.json")
+const TestTokenJson = require("./TestToken.json")
 const OldTokenJson = require("./CrowdsaleToken.json")
 const MarketplaceJson = require("./Marketplace.json")
 const Marketplace2Json = require("./Marketplace2.json")
@@ -79,6 +79,7 @@ function getTestWallet(name, index) {
 async function getProducts() {
     return await (await fetch(`${streamrUrl}/api/v1/products?publicAccess=true`)).json()
 }
+
 function sleep(ms) {
     return new Promise(resolve => {
         setTimeout(resolve, ms)
@@ -103,9 +104,9 @@ class AutoNonceWallet extends Wallet {
 
 
 /**
- * 
+ *
  * From https://github.com/ensdomains/ens/blob/master/migrations/2_deploy_contracts.js
- * 
+ *
  * Calculate root node hashes given the top level domain(tld)
  *
  * @param {string} tld plain text tld, for example: 'eth'
@@ -132,7 +133,7 @@ async function deployUniswap2(wallet) {
     let tx = await deployer.deploy()
     const weth = await tx.deployed()
     log(`WETH deployed to ${weth.address}`)
-    
+
     deployer = new ContractFactory(UniswapV2Factory.abi, UniswapV2Factory.bytecode, wallet)
     tx = await deployer.deploy(wallet.address)
     const factory = await tx.deployed()
@@ -210,10 +211,10 @@ async function smartContractInitialization() {
 
     //another ERC20 that's not datacoin for testing buy with Uniswap
     log(`Deploying test OTHERcoin from ${wallet.address}`)
-    const tokenDeployer2 = new ContractFactory(TokenJson.abi, TokenJson.bytecode, wallet)
-    const tokenDeployTx2 = await tokenDeployer2.deploy("Test OTHERcoin", "\ud83e\udd84")
-    const token2 = await tokenDeployTx2.deployed()
-    
+    const tokenDeployer2 = new ContractFactory(TestTokenJson.abi, TestTokenJson.bytecode, wallet)
+    const tokenDeployTx2 = await tokenDeployer.deploy("Test OTHERcoin", "COIN")
+    const token2 = await tokenDeployTx.deployed()
+
     //Note: TestToken contract automatically mints 100000 to owner
 
     log('Add minter: %s', wallet.address)
@@ -240,25 +241,6 @@ async function smartContractInitialization() {
     await upgradeTx2.wait()
     log('Old token getUpgradeState: %d, expected: 3', await oldToken.getUpgradeState())
 
-    // TODO: commented-out code where each step is awaited
-    // const setupTx1 = await oldToken.setReleaseAgent(signer.address)
-    // await setupTx1.wait()
-    // const setupTx2 = await oldToken.setMintAgent(wallet.address, true)
-    // await setupTx2.wait()
-    // const setupTx3 = await oldToken.mint(wallet.address, oldSupply)
-    // await setupTx3.wait()
-    // log('Old token getUpgradeState: %d, expected: 1', await oldToken.getUpgradeState())
-    // const setupTx4 = await oldToken.releaseTokenTransfer()
-    // await setupTx4.wait()
-    // log('Old token getUpgradeState: %d, expected: 2', await oldToken.getUpgradeState())
-
-    // log('Set migrator as UpgradeAgent => start test-DATAv1 upgrade')
-    // const upgradeTx1 = await token.mint(migrator.address, await oldToken.totalSupply())
-    // await upgradeTx1.wait()
-    // const upgradeTx2 = await oldToken.setUpgradeAgent(migrator.address)
-    // await upgradeTx2.wait()
-    // log('Old token getUpgradeState: %d, expected: 3', await oldToken.getUpgradeState())
-
     log(`Minting ${mintTokenAmount} DATAv2 tokens to following addresses:`)
     for (const address of privateKeys.map(computeAddress)) {
         log("    %s", address)
@@ -273,15 +255,12 @@ async function smartContractInitialization() {
     }
 
     log("Init Uniswap1 factory")
-    let tx = await uniswapFactory.initializeFactory(uniswapExchangeTemplate.address)
-    //await tx.wait()
+    await uniswapFactory.initializeFactory(uniswapExchangeTemplate.address)
     log(`Init Uniswap1 exchange for DATAcoin token ${token.address}`)
-    tx = await uniswapFactory.createExchange(token.address, {gasLimit: 6000000})
-    //await tx.wait()
+    await uniswapFactory.createExchange(token.address, {gasLimit: 6000000})
     log(`Init Uniswap1 exchange for OTHERcoin token ${token2.address}`)
-    tx = await uniswapFactory.createExchange(token2.address, {gasLimit: 6000000})
-    // need wait here to call read methods below
-    await tx.wait()
+    const uniswapTx = await uniswapFactory.createExchange(token2.address, {gasLimit: 6000000})
+    await uniswapTx.wait() // need wait here to call read methods below
 
     let datatoken_exchange_address = await uniswapFactory.getExchange(token.address)
     log(`DATAcoin traded at Uniswap1 exchange ${datatoken_exchange_address}`)
@@ -296,15 +275,11 @@ async function smartContractInitialization() {
     let amt_token = parseEther("1000") // 1 ETH ~= 10 DATAcoin
     let amt_token2 = parseEther("10000") // 1 ETH ~= 100 OTHERcoin
 
-    tx = await token.approve(datatoken_exchange_address, amt_token)
-    //await tx.wait()
-    tx = await token2.approve(othertoken_exchange_address, amt_token2)
-    //await tx.wait()
+    await token.approve(datatoken_exchange_address, amt_token)
+    await token2.approve(othertoken_exchange_address, amt_token2)
 
-    tx = await datatokenExchange.addLiquidity(amt_token, amt_token, futureTime, {gasLimit: 6000000, value: amt_eth})
-    //await tx.wait()
-    tx = await othertokenExchange.addLiquidity(amt_token2, amt_token2, futureTime, {gasLimit: 6000000, value: amt_eth})
-    //await tx.wait()
+    await datatokenExchange.addLiquidity(amt_token, amt_token, futureTime, {gasLimit: 6000000, value: amt_eth})
+    await othertokenExchange.addLiquidity(amt_token2, amt_token2, futureTime, {gasLimit: 6000000, value: amt_eth})
 
     log(`Added liquidity to uniswap exchanges: ${formatEther(amt_token)} DATAcoin, ${formatEther(amt_token2)} OTHERcoin`)
 
@@ -325,7 +300,6 @@ async function smartContractInitialization() {
     log(`1 DATAtoken buys ${formatEther(rate)} ETH`)
     rate = await othertokenExchange.getTokenToEthInputPrice(ethwei)
     log(`1 OTHERtoken buys ${formatEther(rate)} ETH`)
-
 
     //deployment steps based on https://github.com/ensdomains/ens/blob/2a6785c3b5fc27269eb3bb18b9d1245d1f01d6c8/migrations/2_deploy_contracts.js#L30
     log("Deploying ENS")
@@ -354,22 +328,22 @@ async function smartContractInitialization() {
         const domainAddress = addresses[i]
         const fullname = domain + ".eth"
         const fullhash = namehash(fullname)
-        
+
         log(`setting up ENS domain ${domain} with owner ${owner}, pointing to address ${domainAddress}`)
         tx = await fifs.register(Web3.utils.sha3(domain), owner)
         tr = await tx.wait()
         log(`called regsiter`)
-        
+
         tx = await ens.setResolver(fullhash, resolver.address)
         tr = await tx.wait()
         log('called setResolver')
-        
-        //Ethers wont call the 2-arg setAddr. 60 is default = COIN_TYPE_ETH. 
+
+        //Ethers wont call the 2-arg setAddr. 60 is default = COIN_TYPE_ETH.
         //see https://github.com/ensdomains/resolvers/blob/master/contracts/profiles/AddrResolver.sol
         tx = await resolver.setAddr(fullhash, 60, domainAddress)
         tr = await tx.wait()
         log(`called setAddr. done registering ${fullname} as ${domainAddress}`)
-    
+
         //transfer ownership
         tx = await ens.setOwner(fullhash, addresses[i])
         tr = await tx.wait()
@@ -389,15 +363,13 @@ async function smartContractInitialization() {
    const router = await deployUniswap2(wallet)
    log(`deploy Uniswap2 sidechain`)
    await deployUniswap2(sidechainWallet)
-   
+
    tx = await token.approve(router.address, amt_token)
    //await tx.wait()
    tx = await token2.approve(router.address, amt_token2)
    await tx.wait()
    log(`addLiquidity Uniswap2 mainnet`)
-   tx = await router.addLiquidity(token.address, token2.address, amt_token, 
-    amt_token2, 0, 0, wallet.address, futureTime)
-
+   tx = await router.addLiquidity(token.address, token2.address, amt_token, amt_token2, 0, 0, wallet.address, futureTime)
 
    //put additions here
 
