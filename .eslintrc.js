7585ea2b4fea4e6f093cbd61fd581ff62072294c
Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>
2021-11-05 11:16:12 +0400

    fix: got linting to work, fixed lint issues, fixed uninitialised references

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    requireConfigFile: false
+  },
+  rules: {
+  },
+  parser: '@babel/eslint-parser'
+}
